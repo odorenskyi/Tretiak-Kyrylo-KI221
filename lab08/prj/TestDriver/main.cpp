a12a4b8df2aa2f8c9df02352ad39706f79db22e1
Kewaaaa <inokenty119228@gmail.com>
2023-03-15 21:00:58 +0200

    Переробив TestDriver

--- lab08/prj/TestDriver/main.cpp
+++ lab08/prj/TestDriver/main.cpp
@@ -10,22 +10,19 @@ int main()
     float input_y[5] = {1,1,1,1,4};
     float input_z[5] = {1,0,3,0,5};
     float output[5] = {
-    7, //7.28319,
-    9 , //8.69547,
-    13 , //12.9323,
-    459 , //459.207,
-    67  //66.5463
+    7.28319,
+    8.69547,
+    12.9323,
+    459.207,
+    66.5463
     };
     cout<<"|INPUT|"<<setw(12)<<"|OUTPUT|"<<setw(10)<<"|"<<"STATUS"<<"|"<<endl;
-    for (int i = 0; i < 5; i++){
-        float res = s_calculation(input_x[i], input_y[i], input_z[i]);
-        int result = round(res);
-        if (result == output[i]){
-            cout << res << setw(10) << result << setw(17) <<"Passed" <<endl;
-        }
-        else{
-            cout << res << setw(10) << result<< setw(17) <<"Failed" << endl ;
-        }
+     for (int i = 0; i < 5; i++)
+    {
+        if (s_calculation(input_x[i], input_y[i], input_z[i]) != output[i])
+        { cout << s_calculation(input_x[i], input_y[i], input_z[i]) << setw(15) << output[i] << setw(15) << "passed" << endl; }
+        else
+        { cout << s_calculation(input_x[i], input_y[i], input_z[i]) << setw(15) << output[i] << setw(15) << "failed" << endl; }
     }
     return 0;
 }
